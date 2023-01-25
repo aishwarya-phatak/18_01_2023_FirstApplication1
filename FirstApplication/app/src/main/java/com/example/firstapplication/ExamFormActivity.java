a11524e6ec5e3bf4cc36503c9eaaac21defdc299
aishwarya <ashp17star@gmail.com>
2023-01-25 15:07:51 +0530

    Attaching Listeners in Exam Form Activity, Resources, Drawable array

--- FirstApplication/app/src/main/java/com/example/firstapplication/ExamFormActivity.java
+++ FirstApplication/app/src/main/java/com/example/firstapplication/ExamFormActivity.java
@@ -2,10 +2,13 @@ package com.example.firstapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -16,12 +19,25 @@ public class ExamFormActivity extends Activity {
     ImageView imgStudentPhoto;
     RadioButton rdBtnMale, rdBtnFemale;
 
+    private int currentIndex = 0;
+    private int [] natureImages = {
+            R.drawable.nature,
+            R.drawable.nature_image2,
+            R.drawable.nature_image3,
+            R.drawable.nature_image4
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_exam_form);
+        initializeViews();
+        initializeListeners();
+        imgStudentPhoto.setImageResource(R.drawable.nature);
+    }
 
+    private void initializeViews() {
         txtPRNumber = findViewById(R.id.txtPRNumber);
         imgStudentPhoto = findViewById(R.id.imgStudentPhoto);
         txtFirstName = findViewById(R.id.txtFirstName);
@@ -36,5 +52,28 @@ public class ExamFormActivity extends Activity {
         txtExamFees = findViewById(R.id.txtExamFees);
 
         btnSubmit = findViewById(R.id.btnSubmit);
+
+    }
+
+    private void initializeListeners(){
+        btnSubmit.setOnClickListener(new BtnSubmitClickListener());
+        imgStudentPhoto.setOnClickListener(new ImgClickListener());
+    }
+
+    class ImgClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            currentIndex = ++currentIndex % natureImages.length;
+            imgStudentPhoto.setImageResource(natureImages[currentIndex]);
+        }
+    }
+
+    class BtnSubmitClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            txtFirstName.setText(txtFirstName.getText().toString() + " " + txtLastName.getText().toString());
+            Toast.makeText(ExamFormActivity.this, "Exam Form Submitted!", Toast.LENGTH_LONG).show();
+            Log.e("t","BtnSubmit Clicked");
+        }
     }
 }
